f28983b9025571595c9a4bbfa9f2792be65a30e8
Julien ⚜️ <mp.dev.mg@gmail.com>
2025-05-14 12:07:27 +0200

    Add of the HousingTitle component

--- src/pages/HousingPage.jsx
+++ src/pages/HousingPage.jsx
@@ -1,9 +1,13 @@
 import style from './HousingPage.module.scss';
 import HeroCarousel from '../components/HeroCarousel.jsx';
+import TitleHousing from '../components/TitleHousing.jsx';
 export default function HousingPage() {
     return (
         <div className={style}>
             <HeroCarousel />
+            <div>
+                <TitleHousing titleMain={'blablablabla'} titleLocalize={'blablablabla blablablabla blablablabla'}/>
+            </div>
         </div>
     );
 };
