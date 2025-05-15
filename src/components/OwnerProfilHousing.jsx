97249a8610f9317c1b5caa474bcd6c46722dc3ec
Julien ⚜️ <mp.dev.mg@gmail.com>
2025-05-15 20:05:08 +0200

    Debogging the OwerProfilHousing component

--- src/components/OwnerProfilHousing.jsx
+++ src/components/OwnerProfilHousing.jsx
@@ -1,11 +1,14 @@
 import style from './OwnerProfilHousing.module.scss';
 
 export default function OwnerProfilHousing({hostName, hostPicture}){
+    const name = hostName.split(' ')[0];
+    const lastName = hostName.split(' ')[1];
+
     return (
         <section className={ style.OwnerProfilHousing}>
             <h3>
-                <span>{hostName}</span>
-                <span>{hostName}</span>
+                <span>{name}</span>
+                <span>{lastName}</span>
             </h3>
             <img src={hostPicture} alt={`Photo profile of ${hostName}`} />
         </section>
