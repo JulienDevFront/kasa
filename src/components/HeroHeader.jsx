cefb3e4d305bd4feab380d76c38ea3d9e619c5f8
Julien ⚜️ <mp.dev.mg@gmail.com>
2025-05-21 12:25:32 +0200

    Add of the responsive of the component HeroHeader

--- src/components/HeroHeader.jsx
+++ src/components/HeroHeader.jsx
@@ -4,7 +4,9 @@ export default function HeroHeader({title = null, img}){
     return (
         <aside className={style.HeroHeader}>
             <img src={img} alt='Photograph of a landscape'/>
-            <h1>{title}</h1>
+        <h1>{title && title.split(',').map((text, index, array) => 
+            <span key={index}>{text}{index < array.length - 1 && ', '}</span>)}
+        </h1>
         </aside>
     );
 };
